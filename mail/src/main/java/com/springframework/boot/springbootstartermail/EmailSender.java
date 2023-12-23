1a369ca0577cee476f258b340fa924c371e7b3e0
Robin Jünemann <robin@juenemanns.de>
2023-12-23 15:35:45 +0100

    added reference to mail server config

--- mail/src/main/java/com/springframework/boot/springbootstartermail/EmailSender.java
+++ mail/src/main/java/com/springframework/boot/springbootstartermail/EmailSender.java
@@ -1,12 +1,17 @@
 package com.springframework.boot.springbootstartermail;
+import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 
@@ -14,7 +19,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
-@RequestMapping("/testapp")
+@RequestMapping("/test-app")
 @Controller
 public class EmailSender {
     @Autowired
@@ -23,29 +28,45 @@ public class EmailSender {
     @Autowired
     private static JavaMailSender sender;
 
-    @RequestMapping("/getdetails")
-    public static @ResponseBody void sendMail(@RequestBody Details details) throws Exception {
+    @RequestMapping("/get-details")
+    public static @ResponseBody Details sendMail(@RequestBody Details details) throws Exception {
 
         MimeMessage message = sender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message,
                 MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
                 StandardCharsets.UTF_8.name());
-
-        Map<String, Object> model = new HashMap<>();
+        try {
+        Map<String, Object> model = new HashMap<String, Object>();
         model.put("name",details.getName());
         model.put("email",details.getEmail());
         model.put("subject",details.getSubject());
         model.put("message",details.getMessage());
-        model.put("receiver",details.getReceiver());
 
         Context context = new Context();
         context.setVariables(model);
         String html = templateEngine.process("email-template", context);
 
         helper.setTo(details.getEmail());
-        helper.setText(html,true);
+        helper.setText(html, true);
         helper.setSubject("Test Mail");
+    } catch (MessagingException e) {
+        e.printStackTrace();
+    }
         sender.send(message);
+        return details;
+    }
+    @Configuration
+    class AppConfig {
 
+        @Bean
+        public TemplateEngine templateEngine() {
+            return new SpringTemplateEngine();
+        }
+
+        @Bean
+        public JavaMailSender javaMailSender() {
+                return (JavaMailSender) new AppConfig();
+        }
     }
 }
+
--- mail/src/main/java/com/springframework/boot/springbootstartermail/SpringBootStarterMailApplication.java
+++ mail/src/main/java/com/springframework/boot/springbootstartermail/SpringBootStarterMailApplication.java
@@ -1,26 +1,13 @@
 package com.springframework.boot.springbootstartermail;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class SpringBootStarterMailApplication {
-
-	@Autowired
-	private EmailSender senderService;
-
-	private Details details;
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootStarterMailApplication.class, args);
 	}
 
-	@EventListener(ApplicationReadyEvent.class)
-	public void sendEmail() throws Exception {
-		EmailSender.sendMail(details);
-	}
 }
