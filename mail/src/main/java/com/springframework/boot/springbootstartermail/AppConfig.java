8621ee43d89eb86fe94d42240c2ffd1d1baec426
Robin Jünemann <robin@juenemanns.de>
2023-12-23 15:27:48 +0100

    added mail server config

---
+++ mail/src/main/java/com/springframework/boot/springbootstartermail/AppConfig.java
@@ -0,0 +1,42 @@
+package com.springframework.boot.springbootstartermail;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class AppConfig {
+
+    @Value("smtp.gmail.de")
+    private String host;
+
+    @Value("587")
+    private int port;
+
+    @Value("troumble@gmail.com")
+    private String username;
+
+    @Value("xkws dxff wglo qizt")
+    private String password;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setHost(host);
+        javaMailSender.setPort(port);
+        javaMailSender.setUsername(username);
+        javaMailSender.setPassword(password);
+
+        Properties props = javaMailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return javaMailSender;
+    }
+}
